36be576de46e30026af0420351bce708da832941
DanielR <danielrusu8100@gmail.com>
2026-03-30 22:18:07 -0600

    Set up firebase for logging in and storing data, not functional yet.

---
+++ firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+
+import { 
+  getAuth, 
+  onAuthStateChanged,
+  signOut
+} from 
+"https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDLoZ6uEzkvx2UGvUZdZqFoClnZqnE6dH0",
+    authDomain: "mru-cs-hub.firebaseapp.com",
+    projectId: "mru-cs-hub",
+    storageBucket: "mru-cs-hub.firebasestorage.app",
+    messagingSenderId: "376515875493",
+    appId: "1:376515875493:web:529a30b1e079613a69b34c"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Export auth
+export { auth, onAuthStateChanged, signOut };
